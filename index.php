00767ad495eea2e27190d9aabe9d5b25b484026d
jeanraejullianeleaban <jeanraejulliane.leaban@cvsu.edu.ph>
2026-04-19 10:57:59 +0800

    feat: added index.php

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP-Demo Clone</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+            background-color: #f5d6ed;
+            text-align: center;
+        }
+        .container {
+            max-width: 600px;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: #ffffff;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+        a {
+            display: inline-block;
+            margin-top: 15px;
+            padding: 10px 15px;
+            background:  #6a0dad;
+            color: white;
+            text-decoration: none;
+            border-radius: 5px;
+        }
+
+        a:hover {
+            background: #d862ff;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+
+        <h1>PHP-Demo Clone</h1>
+
+        <p>This is a simple PHP demo clone.</p>
+
+        <p><strong>Jean Rae Julliane A. Leaban</strong><br>
+        BSIT 2-A | ITEC-65</p>
+
+        <a href="https://github.com/jeanraejullianeleaban/leaban_phplab" target="_blank">
+        View My GitHub Repository
+        </a>
+    </div>
+</body>
+</html>
